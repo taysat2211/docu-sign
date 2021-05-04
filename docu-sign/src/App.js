7fc19c6beb87876156d7feb7ee926d31cb733d74
taysat2211 <39727394+taysat2211@users.noreply.github.com>
2021-05-05 04:29:11 +0700

    add modal and show pdf file

--- docu-sign/src/App.js
+++ docu-sign/src/App.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 // import Menu from "./components/Menu";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Login from './modules/login/Login'
-import Signup from './modules/signup/Signup'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './modules/login/Login';
+import Signup from './modules/signup/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Detail from "./modules/file-detail/Detail";
+import Detail from './modules/file-detail/Detail';
+import Menu from './components/Menu';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Login} /> 
-          <Route path="/signup" component={Signup}/>
-          <Route path="/file/detail/1" component={Detail}/>
-        </Switch>
-      </Router>
-    </>
-  );
+	return (
+		<Router>
+				<Switch>
+					<Route exact path="/" component={Login} />
+					<Route path="/signup" component={Signup} />
+					<Route path="/file/detail/1" component={Detail} />
+				</Switch>
+		</Router>
+	);
 }
 
 export default App;
