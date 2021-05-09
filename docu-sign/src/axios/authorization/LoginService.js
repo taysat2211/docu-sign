591be3c4442d6bac740d177f1c724511e3b7593e
taysat2211 <39727394+taysat2211@users.noreply.github.com>
2021-05-09 23:44:28 +0700

    merge code home branch to main

---
+++ docu-sign/src/axios/authorization/LoginService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import config from '../../config/default.json';
+
+export const LoginService = async (username, password) => {
+    try {
+        const response = await axios.post(`${config.backendBaseURL}/auth/login`, { username: username, password: password });
+        return response.data;
+    } catch (err) {
+        // Handle Error Here
+        return null;
+    }
+};
+
